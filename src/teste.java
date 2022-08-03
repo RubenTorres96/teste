cc8eca75c8203f1db9dbde7911bc8549deecf78a
rubentorres <rubentorres135@hotmail.com>
2022-08-03 15:48:25 +0100

    MARADO

--- src/teste.java
+++ src/teste.java
@@ -2,5 +2,6 @@ public class teste {
     public static void main(String[] args) {
         System.out.println("teste");
         System.out.println("KONAN OSIRIS");
+        System.out.println("JOAO MARTINS, LIGAR CAMERA");
     }
 }
